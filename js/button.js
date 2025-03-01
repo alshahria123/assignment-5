f7303e3c181a8333c2f96afdd9b0701b8b9c36b5
ProgrammingHgitggggsjdhgitthgjkfgkfjnbvjkhfkhfckfhc <alshahria354@gmail.com>
2025-03-01 23:20:24 +0600

    added the code

---
+++ js/button.js
@@ -0,0 +1,134 @@
+
+document.getElementById("card-1").addEventListener('click', function(){
+    alert('Board updated Successfully');
+    const task = document.getElementById("task-no").innerText;
+    const convertedTask = parseInt(task);
+
+    const sum = convertedTask - 1;
+    document.getElementById("task-no").innerText = sum;
+    const point = document.getElementById("point").innerText;
+    const convertedPoint = parseInt(point);
+    const sum2 = convertedPoint + 1;
+    document.getElementById("point").innerText = sum2;
+
+    const history = document.getElementById("history");
+
+    let date = new Date();
+
+    const p = document.createElement("p");
+    p.innerText = "You have Complete The Task Fix Mobile Button Issue at " + date
+    history.appendChild(p);
+    this.disabled = true;
+    document.getElementById("card-1").classList.add("bg-gray-400");
+})
+document.getElementById("card-2").addEventListener('click', function(){
+    alert('Board updated Successfully');
+    const task = document.getElementById("task-no").innerText;
+    const convertedTask = parseInt(task);
+
+    const sum = convertedTask - 1;
+    document.getElementById("task-no").innerText = sum;
+    const point = document.getElementById("point").innerText;
+    const convertedPoint = parseInt(point);
+    const sum2 = convertedPoint + 1;
+    document.getElementById("point").innerText = sum2;
+
+    const history = document.getElementById("history");
+
+    let date = new Date();
+
+    const p = document.createElement("p");
+    p.innerText = "You have Complete The Task Fix Mobile Button Issue at " + date
+    history.appendChild(p);
+    this.disabled = true;
+    document.getElementById("card-2").classList.add("bg-gray-400");
+})
+document.getElementById("card-3").addEventListener('click', function(){
+    alert('Board updated Successfully');
+    const task = document.getElementById("task-no").innerText;
+    const convertedTask = parseInt(task);
+
+    const sum = convertedTask - 1;
+    document.getElementById("task-no").innerText = sum;
+    const point = document.getElementById("point").innerText;
+    const convertedPoint = parseInt(point);
+    const sum2 = convertedPoint + 1;
+    document.getElementById("point").innerText = sum2;
+
+    const history = document.getElementById("history");
+
+    let date = new Date();
+
+    const p = document.createElement("p");
+    p.innerText = "You have Complete The Task Fix Mobile Button Issue at " + date
+    history.appendChild(p);
+    this.disabled = true;
+    document.getElementById("card-3").classList.add("bg-gray-400");
+})
+document.getElementById("card-4").addEventListener('click', function(){
+    alert('Board updated Successfully');
+    const task = document.getElementById("task-no").innerText;
+    const convertedTask = parseInt(task);
+
+    const sum = convertedTask - 1;
+    document.getElementById("task-no").innerText = sum;
+    const point = document.getElementById("point").innerText;
+    const convertedPoint = parseInt(point);
+    const sum2 = convertedPoint + 1;
+    document.getElementById("point").innerText = sum2;
+
+    const history = document.getElementById("history");
+
+    let date = new Date();
+
+    const p = document.createElement("p");
+    p.innerText = "You have Complete The Task Fix Mobile Button Issue at " + date
+    history.appendChild(p);
+    this.disabled = true;
+    document.getElementById("card-4").classList.add("bg-gray-400");
+})
+document.getElementById("card-5").addEventListener('click', function(){
+    alert('Board updated Successfully');
+    const task = document.getElementById("task-no").innerText;
+    const convertedTask = parseInt(task);
+
+    const sum = convertedTask - 1;
+    document.getElementById("task-no").innerText = sum;
+    const point = document.getElementById("point").innerText;
+    const convertedPoint = parseInt(point);
+    const sum2 = convertedPoint + 1;
+    document.getElementById("point").innerText = sum2;
+
+    const history = document.getElementById("history");
+
+    let date = new Date();
+
+    const p = document.createElement("p");
+    p.innerText = "You have Complete The Task Fix Mobile Button Issue at " + date
+    history.appendChild(p);
+    this.disabled = true;
+    document.getElementById("card-5").classList.add("bg-gray-400");
+})
+document.getElementById("card-6").addEventListener('click', function(){
+    alert('Board updated Successfully');
+    const task = document.getElementById("task-no").innerText;
+    const convertedTask = parseInt(task);
+
+    const sum = convertedTask - 1;
+    document.getElementById("task-no").innerText = sum;
+    const point = document.getElementById("point").innerText;
+    const convertedPoint = parseInt(point);
+    const sum2 = convertedPoint + 1;
+    document.getElementById("point").innerText = sum2;
+
+    const history = document.getElementById("history");
+
+    let date = new Date();
+
+    const p = document.createElement("p");
+    p.innerText = "You have Complete The Task Fix Mobile Button Issue at " + date
+    history.appendChild(p);
+    this.disabled = true;
+    document.getElementById("card-6").classList.add("bg-gray-400");
+    alert("congrates!!! You have completed all the current task");
+})
